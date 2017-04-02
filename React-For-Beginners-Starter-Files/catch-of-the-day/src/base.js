d4dc3d2836f173685535ea1a881ea2cd67e710f0
evaldas <nobodiesxx@gmail.com>
2017-04-02 23:17:00 +0100

    updates

---
+++ React-For-Beginners-Starter-Files/catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+	apiKey: "AIzaSyAN7fBZskDfTCXm0fx1QkqKO4X2wMbhXZU",
+    authDomain: "react-tutorials-dbd48.firebaseapp.com",
+    databaseURL: "https://react-tutorials-dbd48.firebaseio.com",
+});
+
+export default base;
